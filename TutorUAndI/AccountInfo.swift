f33a56e611a201f6cfcba3d00fce59190366a25c
Zeyong Shan <shan27@pdx.edu>
2017-03-01 22:23:51 -0800

    Finished the Profile part

--- TutorUAndI/AccountInfo.swift
+++ TutorUAndI/AccountInfo.swift
@@ -6,72 +6,7 @@
 //  Copyright © 2017 zeyong shan. All rights reserved.
 //
 
-/*
-======================================================================================================
-Examples :  addNewAccountBasicInfo
-
-
--- Have to logIn before addNewAccInfo
--- **It will auto log in the new account after Sign Up
-
-AccountSetting.logInWithEmail("shan27@pdx.edu", "BIANhao5213"){(error) in
-    print(error ?? "log in successfully!")
-    }
-    
-    -- call the function
-let result = AccountInfo.addNewAccountBasicInfo("ZeyongShan", 1, "YYYYMMDD(has to be 8 digits)", "shan27@pdx.edu")
-print(result)
-    
-======================================================================================================
-Examples :  addNewAccountAddressInfo
-
-
--- Have to logIn before addNewAccInfo
-let result = AccountInfo.addNewAccountAddressInfo("ex.No", "ex.St", "ex.no", "exCity", "exState", 000000)
-print(result)
-    
-    
-======================================================================================================
-Examples :   addNewAccountBankInfo
-
-let result = AccountInfo.addNewAccountBankInfo("Example card number", "MMYY(has to be 4 digits)")
-print(result)
-    
-    
-    
-======================================================================================================
-Examples :   getAccountBasicInfo
-
--- Have to logIn before getAccountBasicInfo
--- *All the arguments for completionHandler is optional.
-
-let result = AccountInfo.getAccountBasicInfo(completionHandler:
-    { (name, gender, dob, email) in
-        if name != nil
-        {
-            print("name is: \(name!)")
-        }
-        if gender != nil
-        {
-            print("gender is: \(gender!)")
-        }
-        if dob != nil
-        {
-            print("dob is: \(dob!)")
-        }
-        if email != nil
-        {
-            print("email is: \(email!))")
-        }
-        
-})
-
-print(result)
-
-*/
-
-
-
+// =============Details in the end of the page!!!!!===============
 
 import Foundation
 import Firebase
@@ -104,7 +39,7 @@ class AccountInfo
     }
     
     
-    class func addNewAccountAddressInfo(_ StNo:String, _ StreetName: String, _ Apt:String, _ City: String, _ State: String, _ Zip: Int) -> Bool
+    class func addNewAccountAddressInfo(_ stNo:String, _ streetName: String, _ apt:String, _ city: String, _ state: String, _ zip: Int) -> Bool
     {
         if  user == nil
         {
@@ -113,17 +48,17 @@ class AccountInfo
         }
         
         
-        ref.child("accInfo").child(user!.uid).child("address").child("StNo").setValue(StNo)
-        ref.child("accInfo").child(user!.uid).child("address").child("StName").setValue(StreetName)
-        ref.child("accInfo").child(user!.uid).child("address").child("Apt").setValue(Apt)
-        ref.child("accInfo").child(user!.uid).child("address").child("City").setValue(City)
-        ref.child("accInfo").child(user!.uid).child("address").child("State").setValue(State)
-        ref.child("accInfo").child(user!.uid).child("address").child("Zip").setValue(Zip)
+        ref.child("accInfo").child(user!.uid).child("address").child("StNo").setValue(stNo)
+        ref.child("accInfo").child(user!.uid).child("address").child("StName").setValue(streetName)
+        ref.child("accInfo").child(user!.uid).child("address").child("Apt").setValue(apt)
+        ref.child("accInfo").child(user!.uid).child("address").child("City").setValue(city)
+        ref.child("accInfo").child(user!.uid).child("address").child("State").setValue(state)
+        ref.child("accInfo").child(user!.uid).child("address").child("Zip").setValue(zip)
         return true
     }
     
     
-    class func addNewAccountBankInfo(_ cardNo:String, _ expireDate4Digit:String) -> Bool
+    class func addNewAccountBankInfo(_ cardNo:String, _ expireDate4Digit:String, _ cardHolder:String) -> Bool
     {
         if  user == nil
         {
@@ -134,6 +69,7 @@ class AccountInfo
         
         ref.child("accInfo").child(user!.uid).child("bank").child("cardNo").setValue(cardNo)
         ref.child("accInfo").child(user!.uid).child("bank").child("expDate").setValue(expireDate4Digit)
+        ref.child("accInfo").child(user!.uid).child("bank").child("holder").setValue(cardHolder)
         return true
     }
     
@@ -149,7 +85,69 @@ class AccountInfo
         
         ref.child("accInfo").child(user!.uid).observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
             let value = snapshot.value as? NSDictionary
-            completionHandler(value?["name"] as! String?, value?["gender"] as! Int?, value?["dob"] as! String?, value?["email"] as! String?)
+            let name = value?["name"] as! String?
+            let gender = value?["gender"] as! Int?
+            let dob = value?["dob"] as! String?
+            let email = value?["email"] as! String?
+            completionHandler(name, gender, dob, email)
+        })
+        return true
+    }
+    
+    
+    class func getAccountAddressInfo(completionHandler: @escaping (_ streetNo: String?, _ streetName: String?, _ apt:String?, _ city:String?, _ state: String?, _ zip: Int?) -> ()) -> Bool
+    {
+        if  user == nil
+        {
+            //No user log in right now !!!
+            return false
+        }
+        
+        
+        ref.child("accInfo").child(user!.uid).child("address").observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            let strNo = value?["StNo"] as! String?
+            let strName = value?["StName"] as! String?
+            let apt = value?["Apt"] as! String?
+            let city = value?["City"] as! String?
+            let state = value?["State"] as! String?
+            let zip = value?["Zip"] as! Int?
+            completionHandler(strNo, strName, apt, city, state, zip)
+        })
+        return true
+    }
+    
+    
+    class func getAccountBankInfo(completionHandler: @escaping (_ replacedCardNo:String?, _ expireDate:String?, _ cardHolder:String?) -> ()) -> Bool
+    {
+        if  user == nil
+        {
+            //No user log in right now !!!
+            return false
+        }
+        
+        
+        ref.child("accInfo").child(user!.uid).child("bank").observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            var cardNo = value?["cardNo"] as! String?
+            let expireDate = value?["expDate"] as! String?
+            let cardHolder = value?["holder"] as! String?
+            
+
+            if cardNo != nil
+            {
+                //delete first 12 characters.
+                for _ in 0..<12
+                {
+                    cardNo?.remove(at: (cardNo?.startIndex)!)
+                }
+                //Insert XXXXXXXXXXXX before numbers.
+                let temp = cardNo
+                cardNo = "XXXXXXXXXXXX"
+                cardNo?.append(temp!)
+            }
+            
+            completionHandler(cardNo, expireDate, cardHolder)
         })
         return true
     }
@@ -159,4 +157,116 @@ class AccountInfo
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 
+ =========important=========
+ it may not work if writing the log in part and getAccountInfo part together!
+ because it cannot ensure that log in will be implement first!
+ But it will work if user log in before like 1 second.
+ =========important=========
+ 
+ ======================================================================================================
+ 1.Examples :  addNewAccountBasicInfo
+ 
+ 
+ -- Have to logIn before addNewAccInfo
+ -- **It will auto log in the new account after Sign Up
+ 
+ AccountSetting.logInWithEmail("shan27@pdx.edu", "BIANhao5213"){(error) in
+ print(error ?? "log in successfully!")
+ }
+ 
+ -- call the function
+ let result = AccountInfo.addNewAccountBasicInfo("ZeyongShan", 1, "YYYYMMDD(has to be 8 digits)", "shan27@pdx.edu")
+ print(result)
+ 
+ ======================================================================================================
+ 2.Examples :  addNewAccountAddressInfo
+ 
+ 
+ -- Have to logIn before addNewAccInfo
+ let result = AccountInfo.addNewAccountAddressInfo("ex.No", "ex.St", "ex.no", "exCity", "exState", 000000)
+ print(result)
+ 
+ 
+ ======================================================================================================
+ 3.Examples :   addNewAccountBankInfo
+ 
+ let result = AccountInfo.addNewAccountBankInfo("Example card number", "MMYY(has to be 4 digits)")
+ print(result)
+ 
+ 
+ 
+ ======================================================================================================
+ 4.Examples :   getAccountBasicInfo
+ 
+ -- Have to logIn before getAccountBasicInfo
+ -- *All the arguments for completionHandler is optional.
+ 
+ let result = AccountInfo.getAccountBasicInfo(completionHandler:
+ { (name, gender, dob, email) in
+ if name != nil
+ {
+ print("name is: \(name!)")
+ }
+ if gender != nil
+ {
+ print("gender is: \(gender!)")
+ }
+ if dob != nil
+ {
+ print("dob is: \(dob!)")
+ }
+ if email != nil
+ {
+ print("email is: \(email!))")
+ }
+ 
+ })
+ -- The return value doesn't means we get the value, it only means the user already log in
+ print(result)
+ 
+ 
+ ======================================================================================================
+ 5.Examples :   getAccountAddressInfo
+ 
+ 
+ let result = AccountInfo.getAccountAddressInfo()
+ {(stNo, stName, apt, city, state, zip) in
+ print(stNo ?? "nothing")
+ print(stName ?? "nothing")
+ print(apt ?? "nothing")
+ print(city ?? "nothing")
+ print(state ?? "nothing")
+ print(zip ?? "nothing")
+ 
+ }
+ print(result)
+ 
+ 
+ ======================================================================================================
+ 6.Examples :   getAccountBankInfo
+ 
+ 
+ let result = AccountInfo.getAccountBankInfo()
+ { (cardNo, expDate, holder) in
+ print(cardNo ?? "nothing!!!")  -- already replaced first 12 digits with XXXXXXXXXXXX
+ print(expDate ?? "nothing!!!")
+ print(holder ?? "nothing!!!")
+ }
+ print(result)
+ 
+ */
 
--- TutorUAndI/AccountSetting.swift
+++ TutorUAndI/AccountSetting.swift
@@ -1,35 +1,17 @@
 //
-//  AccountMangement.swift
+//  AccountSetting.swift
 //  TutorUAndI
 //
 //  Created by zeyong shan on 2/28/17.
 //  Copyright © 2017 zeyong shan. All rights reserved.
 //
 
+
+// =============Details in the end of the page!!!!!===============
+
+
 import Foundation
 import Firebase
-
-
-/*
- -- one example for using these functions:
- 
- AccountManagement.signUpWithEmail("example@pdx.edu", "examplepassword", completionHandler: {(error) in
- print(error ?? "sign up successfully!")
- })
- 
- -- the completionHandler can also be took out from the argument list:
- 
- AccountManagement.signUpWithEmail("sdfsdf@pdf.cd", "hahahaaha"){ (error) in
- print(error ?? "sign up successfully!")
- }
- 
- 
- -- pass in these functions with a completionHandler instead of catch the return value.
- -- the type of completionHandler is a function which take string as argument and return nothing.
- -- if the string is nil, which means the operation successful, if it is not, which means some error in there
- and the error message was stored in the string.
- 
- */
 
 
 class AccountSetting
@@ -78,3 +60,41 @@ class AccountSetting
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ -- one example for using these functions:
+ 
+ AccountSetting.signUpWithEmail("example@pdx.edu", "examplepassword", completionHandler: {(error) in
+ print(error ?? "sign up successfully!")
+ })
+ 
+ -- the completionHandler can also be took out from the argument list:
+ 
+ AccountSetting.signUpWithEmail("sdfsdf@pdf.cd", "hahahaaha"){ (error) in
+ print(error ?? "sign up successfully!")
+ }
+ 
+ 
+ -- pass in these functions with a completionHandler instead of catch the return value.
+ -- the type of completionHandler is a function which take string as argument and return nothing.
+ -- if the string is nil, which means the operation successful, if it is not, which means some error in there
+ and the error message was stored in the string.
+ 
+ */
+
