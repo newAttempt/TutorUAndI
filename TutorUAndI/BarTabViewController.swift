3055297c1b880657b69f0ebc8cfe8c5a239476b0
Tutu Yin <xiaolei@pdx.edu>
2017-03-08 23:55:41 -0800

    updating tab bar view

---
+++ TutorUAndI/BarTabViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BarTabViewController.swift
+//  TutorUAndI
+//
+//  Created by Tutu Wei on 3/8/17.
+//  Copyright © 2017 zeyong shan. All rights reserved.
+//
+
+import UIKit
+
+class BarTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TutorUAndI/MyAccountViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MyAccountViewController.swift
+//  TutorUAndI
+//
+//  Created by Tutu Wei on 3/8/17.
+//  Copyright © 2017 zeyong shan. All rights reserved.
+//
+
+import UIKit
+
+class MyAccountViewController: UITabBarController  {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+           }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    }
